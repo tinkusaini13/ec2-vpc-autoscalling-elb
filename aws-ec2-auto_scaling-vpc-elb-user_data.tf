fbc4d579d48b9e6fbf3c0eec083c8faf0903baa4
Tinku Saini <88707521+tinkusaini13@users.noreply.github.com>
2023-03-16 14:02:48 +0530

    Create aws-ec2-auto_scaling-vpc-elb-user_data.tf

---
+++ aws-ec2-auto_scaling-vpc-elb-user_data.tf
@@ -0,0 +1,135 @@
+provider "aws" {
+  region = "us-west-2" # Update with your desired region
+  access_key = "AKIA88d8d8he4SHR"
+  secret_key = "X283782yd8wrhgfhryfpKPWyfYUdxZQ7lTw"
+
+  default_tags {
+    tags = {
+      hashicorp-learn = "aws-asg"
+    }
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.77.0"
+
+  name = "main-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+data "aws_ami" "amazon-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*-x86_64-ebs"]
+  }
+}
+
+resource "aws_launch_configuration" "demo" {
+  name_prefix     = "terraform-aws-asg-"
+  image_id        = data.aws_ami.amazon-linux.id
+  instance_type   = "t2.micro"
+  user_data       = file("user-data.sh")
+  security_groups = [aws_security_group.demo_instance.id]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "demo" {
+  name                 = "demo"
+  min_size             = 1
+  max_size             = 3
+  desired_capacity     = 1
+  launch_configuration = aws_launch_configuration.demo.name
+  vpc_zone_identifier  = module.vpc.public_subnets
+
+  tag {
+    key                 = "Name"
+    value               = "demo_asg"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "demo" {
+  name               = "arn-asg-demo-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.demo_lb.id]
+  subnets            = module.vpc.public_subnets
+}
+
+resource "aws_lb_listener" "demo" {
+  load_balancer_arn = aws_lb.demo.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.demo.arn
+  }
+}
+
+resource "aws_lb_target_group" "demo" {
+  name     = "learn-asg-demo"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+
+resource "aws_autoscaling_attachment" "demo" {
+  autoscaling_group_name = aws_autoscaling_group.demo.id
+  alb_target_group_arn   = aws_lb_target_group.demo.arn
+}
+
+resource "aws_security_group" "demo_instance" {
+  name = "learn-asg-demo-instance"
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.demo_lb.id]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.demo_lb.id]
+  }
+
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_security_group" "demo_lb" {
+  name = "learn-asg-demo-lb"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = module.vpc.vpc_id
+}
